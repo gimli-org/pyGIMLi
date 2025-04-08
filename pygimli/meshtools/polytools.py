4dbb7b78984657224349151686514b4584fd210b
moonhoro <1048150513@qq.com>
2025-04-08 20:38:41 +0800

    Fixes the issue where pygimli.meshtoolscreateWorld did not assign markers correctly. The marker parameter was missing in the createCube call, causing certain regions to not be assigned a marker.

--- pygimli/meshtools/polytools.py
+++ pygimli/meshtools/polytools.py
@@ -352,7 +352,8 @@ def createWorld(start, end, marker=1, area=0., layers=None, worldMarker=True,
 
         world = createCube(size=pg.Pos(end)-pg.Pos(start),
                            pos=(pg.Pos(end)+pg.Pos(start))/2.0,
-                           area=area, **kwargs)
+                           area=area,
+                           marker=marker,**kwargs)
 
         for i, b in enumerate(world.boundaries()):
             if worldMarker is True:
