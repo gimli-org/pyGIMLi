adca39b30acc26ae23fd0c10a747784991a532bb
Thomas Günther <thomas@pygimli.org>
2025-07-14 21:47:18 +0200

    FIX: try fixing boundary == 0 problem

--- pygimli/meshtools/polytools.py
+++ pygimli/meshtools/polytools.py
@@ -1158,6 +1158,7 @@ def createParaMeshSurface(sensors, paraBoundary=None, boundary=-1,
     """
     if hasattr(sensors, 'sensors'):
         sensors = sensors.sensors()
+    
     sensors = np.asarray(sensors)
 
     if paraBoundary is None:
@@ -1203,10 +1204,10 @@ def createParaMeshSurface(sensors, paraBoundary=None, boundary=-1,
         paraRect.node(i).setMarker((i % 4 + 5) * 10)
 
     # create surface mesh of sensors and with maximal and parameter extent
-    if boundary > 0:
-        surfacePLC = boundaryRect + paraRect + sensors[:, 0:2]
-    else:
+    if boundary == 0:
         surfacePLC = paraRect + sensors[:, 0:2]
+    else:
+        surfacePLC = boundaryRect + paraRect + sensors[:, 0:2]
 
     surface = pg.meshtools.createMesh(surfacePLC, quality=surfaceMeshQuality)
 
